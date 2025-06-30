995909d7d84390625f64de6a37a34ee7eb9d3f76
Marco Behler <marco@jetbrains.com>
2025-06-30 14:01:40 +0200

    initial comit

---
+++ src/main/java/org/example/springbootpodcasat/SpringbootPodcasatApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootpodcasat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPodcasatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPodcasatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootpodcasat/SpringbootPodcasatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootpodcasat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPodcasatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
